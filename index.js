a7ddd0dce067a8659df034052f7a1b8534951e01
Nadine Grantz <nadine.grantz@googlemail.com>
2023-12-14 11:51:10 +0100

    change show answer to hide answer

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ bookmarkButton.addEventListener("click", () => {
     bookmarkButton.classList.toggle("bookmark--active");
 });
 
-// Answer Button
+// Answer Button 1.1 (zeig mir die Antwort)
 
 const answer = document.querySelector('[data-js="answer"]');
 const showAnswerButton = document.querySelector('[data-js="showAnswerButton"]');
@@ -25,5 +25,20 @@ showAnswerButton.addEventListener("click", () => {
 answer.style.display = 'none';
    }
 });
+ 
+// Answer Button 1.2 (ändere den Text "show answer" zu "hode answer")
 
+const hideShowSwitchButton = document.querySelector('[data-js="showAnswerButton"]');
+
+hideShowSwitchButton.addEventListener("click", (event) => {
+  const currentButton = event.currentTarget;
+  const currentText = currentButton.textContent;
+
+  if (currentText === 'Show Answer') {
+    currentButton.textContent = 'Hide Answer';
+  } else {
+    currentButton.textContent = 'Show Answer';
+  }
+});
+  
  
