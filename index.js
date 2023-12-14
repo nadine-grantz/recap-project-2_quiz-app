fcca2ea034c6db63d1a6df6cf017f230886eabea
Nadine Grantz <nadine.grantz@googlemail.com>
2023-12-14 15:11:48 +0100

    change solution

--- index.js
+++ index.js
@@ -8,37 +8,18 @@ bookmarkButton.addEventListener("click", () => {
     bookmarkButton.classList.toggle("bookmark--active");
 });
 
-// Answer Button 1.1 (zeig mir die Antwort)
+// Answer Button 1.1 (zeig mir die Antwort) + Answer Button 1.2 (ändere den Text "show answer" zu "hide answer")
 
 const answer = document.querySelector('[data-js="answer"]');
 const showAnswerButton = document.querySelector('[data-js="showAnswerButton"]');
 
-// probieren ob data-js richtig eingebunden ist console.log("hello");
-
-// funktioniert auch: showAnswerButton.addEventListener("click", () => {
-//answer.classList.toggle("card__answer--active");
-
 showAnswerButton.addEventListener("click", () => {
-   if (answer.style.display === 'none') {
-    answer.style.display = 'block';
-   } else {
-answer.style.display = 'none';
-   }
-});
- 
-// Answer Button 1.2 (ändere den Text "show answer" zu "hode answer")
+    answer.classList.toggle("hidden");
 
-const hideShowSwitchButton = document.querySelector('[data-js="showAnswerButton"]');
-
-hideShowSwitchButton.addEventListener("click", (event) => {
-  const currentButton = event.currentTarget;
-  const currentText = currentButton.textContent;
-
-  if (currentText === 'Show Answer') {
-    currentButton.textContent = 'Hide Answer';
+    if (showAnswerButton.textContent === "Hide Answer") {
+    showAnswerButton.textContent = "Show Answer";
   } else {
-    currentButton.textContent = 'Show Answer';
+    showAnswerButton.textContent = "Hide Answer";
   }
-});
-  
- 
+    //console.log("bisasam");
+}); 
