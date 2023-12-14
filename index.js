830f92cc860e10eb7d7b6a296251b1436422a665
Nadine Grantz <nadine.grantz@googlemail.com>
2023-12-14 11:21:58 +0100

    add show_hide_button

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// Toogle Button
+
 const bookmarkButton = document.querySelector('[data-js="bookmark_button"]');
 
 // probieren ob js richtig eingebunden ist: console.log("probieren");
@@ -5,3 +7,23 @@ const bookmarkButton = document.querySelector('[data-js="bookmark_button"]');
 bookmarkButton.addEventListener("click", () => {
     bookmarkButton.classList.toggle("bookmark--active");
 });
+
+// Answer Button
+
+const answer = document.querySelector('[data-js="answer"]');
+const showAnswerButton = document.querySelector('[data-js="showAnswerButton"]');
+
+// probieren ob data-js richtig eingebunden ist console.log("hello");
+
+// funktioniert auch: showAnswerButton.addEventListener("click", () => {
+//answer.classList.toggle("card__answer--active");
+
+showAnswerButton.addEventListener("click", () => {
+   if (answer.style.display === 'none') {
+    answer.style.display = 'block';
+   } else {
+answer.style.display = 'none';
+   }
+});
+
+ 
